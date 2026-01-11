abfa6f5f8d94f5db0d484c44b3049a3be4832723
R4IN <logged2fa@gmail.com>
2026-01-11 00:11:21 -0600

    Create TEST.php

---
+++ port@server/root.access/TEST.php
@@ -0,0 +1,6 @@
+#EXTM3U
+#EXTINF:-1 group-title="Brisanet BR", BAND
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"bf4ed0d53f284514a5724052736edd13":"b374c809058541966bed5e4716af55f4"}
+https://cdn-brisanet.cportal.com.br/BANDEIRANT/DASH/stream.mpd
